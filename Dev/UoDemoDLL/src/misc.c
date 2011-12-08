b6e888a610b8e1d23a127dd98a9a9a49807694b0
derrick <derrick@4bf54c5a-0cb7-ff49-8398-c775ed83c514>
2011-12-08 17:27:50 +0000

    Previous patch was sign extending the comparison, did not work. This fixes it to use 127ms, but the threshold is too low. Looking into better alternatives.

--- Dev/UoDemoDLL/src/misc.c
+++ Dev/UoDemoDLL/src/misc.c
@@ -9,15 +9,24 @@ void Initialize_misc(void);
 
 // This is a patch for the timing warning regarding DoTick duration. 
 // The function expects time in Seconds, but gets time in MS due to a porting bug. 
-// This changes the threshold from 10ms to 255 ms
+// This changes the (incorrect) threshold from 10ms to 255 ms
 PATCHINFO PI_FixTimingErrors =
 {
  0x004682DD,
- 8, {0x83, 0xF8, 0x0A, 0x76, 0x24, 0x68, 0x40, 0xB7}, // cmp     eax, 10
- 8, {0x83, 0xF8, 0xFF, 0x76, 0x24, 0x68, 0x40, 0xB7}, // cmp     eax, 255
+ 8, {0x83, 0xF8, 0x0A, 0x76, 0x24, 0x68, 0x40, 0xB7}, // cmp     eax, 10 ...
+ 8, {0x83, 0xF8, 0x7F, 0x76, 0x24, 0x68, 0x40, 0xB7}, // cmp     eax, 127 ...
 };
+
+PATCHINFO PI_FixTimingMessage =
+{
+ 0x0061B75E,
+ 12, {"10 seconds."},
+ 12, {"127 ms....."},
+};
+
 
 void Initialize_misc()
 {
 	Patch(&PI_FixTimingErrors);
+	Patch(&PI_FixTimingMessage);
 }
