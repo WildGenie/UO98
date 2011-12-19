9ab19b000a4e658736f5b0f5cf31bdfebb503c8d
derrick <derrick@4bf54c5a-0cb7-ff49-8398-c775ed83c514>
2011-12-19 20:53:51 +0000

    Added new project which will serve as a persistent service container for the UO:98 server.

---
+++ Dev/UO98/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+
+namespace UO98
+{
+    class Program
+    {
+        static ServerProcess process;
+
+        static void Main(string[] args)
+        {
+            process = new ServerProcess(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location));
+            process.Start();
+            while(true)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+
+
+    }
+
+}
---
+++ Dev/UO98/ServerProcess.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace UO98
+{
+    class ServerProcess
+    {
+        readonly string BinDirectory = string.Empty;
+        readonly string UODemoPlusFilename = "UODemo+.exe";
+
+        static readonly Dictionary<string, string> EnvVars = new Dictionary<string, string>()
+        {
+            {"REGKEYNAME","UO98"},
+            {"SERVERNAME","UO98"},
+            {"RUNDIR","rundir"},
+            {"REALDAMAGE","YES"},
+            {"USEACCOUNTNAME","YES"},
+            {"SAVEDYNAMIC0","NO"},
+            {"UODEMODLL","Sidekick.dll"},
+        };
+
+        bool IsExiting { get; set; }
+
+        Process MyProcess;
+
+        public ServerProcess(string workingfolder)
+        {
+            BinDirectory = workingfolder;
+            EnvVars["UODEMODLL"] = Path.Combine(BinDirectory, EnvVars["UODEMODLL"]);
+        }
+
+        public void Start()
+        {
+            if(!IsRunning)
+            {
+                IsExiting = false;
+                Run();
+            }
+        }
+
+        public void Stop()
+        {
+            IsExiting = true;
+            if(MyProcess != null)
+            {
+                MyProcess.Kill();
+            }
+        }
+
+        bool IsRunning { get; set; }
+        bool ShouldRun { get { return MyProcess == null || !IsExiting || !IsRunning; } }
+
+        Process CreateProcess()
+        {
+            var p = new Process();
+            SetUpEnvironment(p.StartInfo);
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.FileName = Path.Combine(BinDirectory, UODemoPlusFilename);
+            return p;
+        }
+
+        void SetUpEnvironment(ProcessStartInfo pInfo)
+        {
+            foreach(KeyValuePair<string, string> var in EnvVars)
+                pInfo.EnvironmentVariables[var.Key] = var.Value;
+        }
+
+        void Run()
+        {
+            while(ShouldRun)
+            {
+                MyProcess = CreateProcess();
+                MyProcess.Start();
+
+                while(IsRunning = !MyProcess.HasExited)
+                    Thread.Sleep(1000);
+            }
+            MyProcess = null;
+        }
+    }
+}
